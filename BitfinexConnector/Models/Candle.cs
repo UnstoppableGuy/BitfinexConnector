4445380c494cbed950805e6fe94c18aa11a4836d
UnstoppableGuy <pavel.takynov@gmail.com>
2025-06-14 13:40:56 +0300

    add base models

---
+++ BitfinexConnector/Models/Candle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitfinexConnector.Models
+{
+    public class Candle
+    {
+        public DateTimeOffset Timestamp { get; set; }
+        public decimal Open { get; set; }
+        public decimal Close { get; set; }
+        public decimal High { get; set; }
+        public decimal Low { get; set; }
+        public decimal Volume { get; set; }
+        public string Pair { get; set; }
+    }
+}
---
+++ BitfinexConnector/Models/Ticker.cs
@@ -0,0 +1,17 @@
+﻿namespace BitfinexConnector.Models
+{
+    public class Ticker
+    {
+        public string Pair { get; set; }
+        public decimal BidPrice { get; set; }
+        public decimal BidSize { get; set; }
+        public decimal AskPrice { get; set; }
+        public decimal AskSize { get; set; }
+        public decimal DailyChange { get; set; }
+        public decimal DailyChangeRelative { get; set; }
+        public decimal LastPrice { get; set; }
+        public decimal Volume { get; set; }
+        public decimal High { get; set; }
+        public decimal Low { get; set; }
+    }
+}
